836ed901b3a478b78f9312faaf2ded574a759f33
Kumar Abhishek <4kumar.abhishek@gmail.com>
2024-02-28 21:44:48 +0530

    Add root route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const wrapAsync = require("./utils/wrapAsync.js");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
